d26ac7ed435857e35cd82c1683a055d63af61801
Italo Daldegan <italodaldegan@hotmail.com>
2018-07-26 13:38:43 -0300

    Fix lint issues

--- dataviva/static/js/modules/ui/app/components/elements/selector.vue
+++ dataviva/static/js/modules/ui/app/components/elements/selector.vue
@@ -1,15 +1,15 @@
 <template>
   <div id="selector">
-    Vue selector application
+    Vue selector
   </div>
 </template>
 
 <script>
-  export default {
-    name: 'Selector',
-    data() {
-      return {
-      };
-    }
-  };
+export default {
+  name: "Selector",
+  data() {
+    return {
+    };
+  },
+};
 </script>
--- dataviva/static/js/modules/ui/app/main.js
+++ dataviva/static/js/modules/ui/app/main.js
@@ -1,9 +1,9 @@
-import Vue from 'vue';
-import * as Components from './components/**/*.vue';
+import Vue from "vue";
+import * as Components from "./components/**/*.vue";
 
 Object.values(Components).map(a => a.name && Vue.component(a.name, a));
+const selector = new Vue({ render: h => h("Selector") });
 
-if (document.getElementById('vue-selector') != null) {
-	var selector = new Vue({ render: h => h('Selector') })
-	selector.$mount('vue-selector');
+if (document.getElementById("vue-selector") != null) {
+  selector.$mount("vue-selector");
 }
